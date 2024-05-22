5f6c374f1fe989d5bc704d0ac5783122a8589aab
Katherine024z <00045724@uca.edu.sv>
2024-05-22 12:50:09 -0600

    comp

--- array3.cpp
+++ array3.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 int main()
 {
-    int numero[100];
-    numero[0]=1;
+    int numero[20];
+    int sumatoria = 0;
     cout << "Sumatoria de los numeros del 1 al 100\n";
 
-    for (int i = 1; i<100; i++ )
+    for (int i = 1; i<21; i++ )
     {
-        cout << i++ << "\n";
+        numero[i];
+        cout << numero[i] << "\n";
+        sumatoria= sumatoria +numero[i];
     }
+    cout << sumatoria;
     return 0;
 }
